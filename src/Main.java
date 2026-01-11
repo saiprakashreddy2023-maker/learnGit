7d11c16bfeb2582f2e11b62143fb46c073bf18af
saiprakashreddy2023-maker <saiprakashreddy2023@gmail.com>
2026-01-11 13:30:21 +0530

    review comments fixed

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,9 @@ public class Main {
         System.out.printf("Hello and welcome!");
         // I am working on the feature changes
         for (int i = 1; i <= 5; i++) {
-
             System.out.println("i = " + i);
         }
         for (int i = 1; i <= 5; i++) {
-
             System.out.println("i = " + i);
         }
     }
