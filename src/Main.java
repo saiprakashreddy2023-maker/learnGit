56a13c1f7887a773ab1810d91092a668798fa505
saiprakashreddy2023-maker <saiprakashreddy2023@gmail.com>
2026-01-21 13:21:20 +0530

    added prime number logic

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,23 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
-            System.out.printf("Hello welcome to github");
-            //I am changed here
+        int num = 29;
+        boolean isPrime = true;
 
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
         }
+
+        System.out.println(isPrime ? "Prime Number" : "Not Prime Number");
     }
+
+
+}
 
