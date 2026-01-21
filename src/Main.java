5e5848de89cb21a9e1e80848f41acf2c9a81f45e
saiprakashreddy2023-maker <saiprakashreddy2023@gmail.com>
2026-01-21 13:24:56 +0530

    fibonokki logic

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.print("Enter number of terms: ");
-        int n = sc.nextInt();
+        int n = 56;
 
         int a = 0, b = 1;
 
